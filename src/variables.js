dbb511f7ac95bcb3f200f2cfa49fbf53a9e89255
Piotrek <65945270+Piotrek-hub@users.noreply.github.com>
2022-02-07 20:15:16 +0100

    final

--- src/variables.js
+++ src/variables.js
@@ -1,7 +1,7 @@
 import { abi } from '../artifacts/contracts/Nft.sol/NFT.json';
 
 const variables = {
-    contractAddress: '0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9',
+    contractAddress: '0xdCf642fc3662Eeb50f1A465F926918C2dbf77F34',
     abi: abi
 }
 
